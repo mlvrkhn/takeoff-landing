29735cfe652a76bc1d5e64adc5902f95f49d3a4b
mlvrkhn <martin.guide@gmail.com>
2024-05-28 00:03:17 +0200

    chore: Add @nuxtjs/tailwindcss dependency and configure it in nuxt.config.ts, update LandingPage.vue with search input and styling

--- components/LandingPage.vue
+++ components/LandingPage.vue
@@ -1,7 +1,23 @@
 <template>
-	<div>
+	<div class="mt-4">
 		<ClientOnly fallback-tag="span" fallback="Loading 3D Globe...">
+			<div class="flex flex-col items-center justify-center min-h-15">
+				<h1 class="text-4xl font-bold mb-4 text-center">
+					Takeoff & Landing
+				</h1>
+				<input
+					type="text"
+					placeholder="Start typing to find airports you visited"
+					class="input input-bordered w-full max-w-xs p-2 rounded-lg text-center"
+					v-model="search"
+				/>
+			</div>
 			<GlobeGl />
 		</ClientOnly>
 	</div>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+const search = ref('');
+</script>

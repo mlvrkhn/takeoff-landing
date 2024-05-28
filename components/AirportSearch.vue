057be1dab322c3971990e6c84b66ab058fbf5d57
mlvrkhn <martin.guide@gmail.com>
2024-05-28 20:36:28 +0200

    abstract search

---
+++ components/AirportSearch.vue
@@ -0,0 +1,53 @@
+<template>
+	<div class="flex flex-col items-center justify-center min-h-15">
+		<h1 class="text-4xl font-bold mb-4 text-center">Takeoff & Landing</h1>
+		<input
+			type="text"
+			placeholder="Start typing to find airports you visited"
+			class="input input-bordered w-full max-w-xs p-2 text-center"
+			v-model="search"
+			@input="handleInput"
+		/>
+	</div>
+	<div class="flex flex-col items-center justify-center">
+		<select
+			class="select select-bordered w-full p-2 max-w-xs"
+			v-if="airports.length"
+		>
+			<option
+				v-for="airport in airports"
+				:key="airport.iata"
+				@click="selectAirport(airport)"
+				selected
+			>
+				{{ airport.iata || '000' }}: {{ airport.name }}
+			</option>
+		</select>
+	</div>
+</template>
+
+<script setup lang="ts">
+const config = useRuntimeConfig();
+import { ref } from 'vue';
+
+const search = ref('');
+let airports = ref([]);
+
+async function handleInput() {
+	airports = await $fetch(
+		'https://airports-by-api-ninjas.p.rapidapi.com/v1/airports?name=' +
+			search.value,
+		{
+			method: 'GET',
+			headers: {
+				'x-rapidapi-host': 'airports-by-api-ninjas.p.rapidapi.com',
+				'x-rapidapi-key': config?.public.apiKey,
+			},
+		}
+	);
+}
+
+function selectAirport(airport) {
+	console.log(airport);
+}
+</script>
--- components/LandingPage.vue
+++ components/LandingPage.vue
@@ -1,42 +1,10 @@
 <template>
 	<div class="mt-4">
 		<ClientOnly fallback-tag="span" fallback="Loading 3D Globe...">
-			<div class="flex flex-col items-center justify-center min-h-15">
-				<h1 class="text-4xl font-bold mb-4 text-center">
-					Takeoff & Landing
-				</h1>
-				<input
-					type="text"
-					placeholder="Start typing to find airports you visited"
-					class="input input-bordered w-full max-w-xs p-2 rounded-lg text-center"
-					v-model="search"
-					@input="handleInput"
-				/>
-			</div>
+			<AirportSearch />
 			<GlobeGl />
 		</ClientOnly>
 	</div>
 </template>
 
-<script setup lang="ts">
-import { ref } from 'vue';
-const config = useRuntimeConfig();
-
-const search = ref('');
-
-async function handleInput() {
-	const data = await $fetch(
-		'https://airports-by-api-ninjas.p.rapidapi.com/v1/airports?name=London',
-		{
-			method: 'GET',
-			headers: {
-				'x-rapidapi-host': 'airports-by-api-ninjas.p.rapidapi.com',
-				'x-rapidapi-key': config?.public.apiKey,
-			},
-		}
-	);
-	console.log('PROCESS:', config);
-
-	console.log('API data:', data);
-}
-</script>
+<script setup lang="ts"></script>
